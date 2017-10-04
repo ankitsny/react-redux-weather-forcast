c7d449fc0702a58e5fbb71447ae92dceefa7e9fe
Ankit Kumar <ankit@unobridge.com>
2017-10-04 22:56:06 +0530

    added style loaders|css-loader|sass-loader

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 console.log('Hello World');
 const x = {};
 console.log('yo');
-
+const css = require('./app.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 
 const join = require('path').join;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
@@ -9,14 +10,32 @@ module.exports = {
     filename: 'app.bundle.js',
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextWebpackPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+          publicPath: '/dist',
+        }),
+      }, // use | loader | loaders 
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Hello World1',
       minify: {
-        collapseWhitespace: true,
+        collapseWhitespace: false,
       },
       hash: true,
       template: './src/index.html',
+    }),
+    new ExtractTextWebpackPlugin({
+      filename: 'app.css',
+      disable: false,
+      allChunks: true,
     }),
   ],
 };
